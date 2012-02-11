0de7bb168b9a12522838ab4dd09b78254ccc84cd
Brian Jackson <bjackson@gmail.com>
2012-02-11 12:46:30 -0600

    testing the github repository for a different project

--- app/controllers/bookshelf/projects_controller.rb
+++ app/controllers/bookshelf/projects_controller.rb
@@ -2,7 +2,7 @@ module Bookshelf
 
   class ProjectsController < ApplicationController
 
-    load_and_authorize_resource
+    load_and_authorize_resource # cancan authorizes that the user has access to the project resources
 
     # GET /projects
     # GET /projects.json
