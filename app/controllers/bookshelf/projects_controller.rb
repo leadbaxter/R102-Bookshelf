07d26771cfc0dbab466dc1c1cad72fb3331b52c5
Brian Jackson <bjackson@gmail.com>
2012-07-04 16:06:47 -0500

    added Bookshelf:: namespace to Bookshelf::Project

--- app/controllers/bookshelf/projects_controller.rb
+++ app/controllers/bookshelf/projects_controller.rb
@@ -2,7 +2,7 @@ module Bookshelf
 
   class ProjectsController < ApplicationController
 
-    load_and_authorize_resource # cancan authorizes that the user has access to the project resources
+    load_and_authorize_resource :class => 'Bookshelf::Project' # cancan authorizes that the user has access to the project resources
 
     # GET /projects
     # GET /projects.json
