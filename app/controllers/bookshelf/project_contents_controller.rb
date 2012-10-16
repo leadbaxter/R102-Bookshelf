d8ec9e73f77087177b659bf036c5c143db424cde
Brian Jackson <bjackson@gmail.com>
2012-10-16 12:48:20 -0500

    add/update projects, add project_content - first pass, using project.id

--- app/controllers/bookshelf/project_contents_controller.rb
+++ app/controllers/bookshelf/project_contents_controller.rb
@@ -1,6 +1,8 @@
 module Bookshelf
   class ProjectContentsController < ApplicationController
 
+    before_filter :check_params, :only => [:create]
+    
     # GET /project_contents
     # GET /project_contents.json
     def index
@@ -9,6 +11,7 @@ module Bookshelf
       respond_to do |format|
         format.html { render :text => "project: #{@project.name}, count: #{@contents.count}"} # index.html.haml
         format.json { render :json => @contents }
+        format.xml  { render :xml => @project }
       end
     end
 
@@ -40,25 +43,109 @@ module Bookshelf
     end
 
     def create
+      #t.references :project
+      #t.string :created_by  # user email
+      #t.text :body
+      #t.text :comments
+      @project = find_project
       respond_to do |format|
-        #format.xml { render xml: Struct.new(:message, :status).new("Fake project status", true) }
-        #format.xml { render xml: { message: "Fake project status", status: true } }
         format.xml {
-          if params[:project] and params[:project][:metadata]
-            the_project = params[:project]
-            the_owner_id = the_project[:metadata][:owner][:id]
-            the_owner_name = the_project[:metadata][:owner][:name]
-            the_project_id = the_project[:metadata][:project][:id]
-            the_project_name = the_project[:metadata][:project][:name]
-            puts "project metadata [owner_id=#{the_owner_id}, owner_name=#{the_owner_name}, project_id=#{the_project_id}, project_name=#{the_project_name}]"
+          if save_content
+            render :text => @content.body, :content_type => 'application/xml'
           else
-            puts "project metadata is absent!"
+            render :text => @errors_xml, :status => :unprocessable_entity
           end
-          render :text => request.body.read, :content_type => 'application/xml'
         }
       end
     end
 
+  private
+    def clip_value sym, init_obj, fld
+      param_obj = params[:project][:metadata][sym]
+      @project_metadata[sym][fld] = param_obj[fld]
+      if fld == :id and param_obj[fld] == init_obj[fld]
+        @project_metadata[sym][:new] = true
+      end
+    end
+
+    def clip_object sym
+      (@project_metadata ||= {})[sym] = {}
+      init_obj = {
+          id: "**cloud_#{sym}_id**", name: "**cloud #{sym}**"
+      }
+      clip_value(sym, init_obj, :id)
+      clip_value(sym, init_obj, :name)
+    end
+
+    # Process the params. Indicate an error if the params don't have project metadata.
+    def check_params
+      if params[:project] and params[:project][:metadata]
+        @project_xml = request.body.read
+        #params[:project][:metadata][:project][:id] = 16
+        clip_object :project
+        clip_object :owner
+        meta_owner = @project_metadata[:owner]
+        meta_project = @project_metadata[:project]
+        puts "project metadata [owner_id=#{meta_owner[:id]}, owner_name=#{meta_owner[:name]}, project_id=#{meta_project[:id]}, project_name=#{meta_project[:name]}]"
+        puts "this is a new project" if @project_metadata[:project][:new]
+        @errors_xml = nil
+      else
+        @errors_xml = "error: project metadata is absent!"
+      end
+    end
+
+    def find_project
+      if @errors_xml.present?
+        nil
+      else
+        meta_owner = @project_metadata[:owner]
+        meta_project = @project_metadata[:project]
+        if meta_project[:new]
+          #t.string :name
+          #t.string :created_by
+          #t.string :content_id
+          #t.boolean :archived
+          Bookshelf::Project.new({ name: meta_project[:name], created_by: meta_owner[:id]})
+        else
+          Bookshelf::Project.find(meta_project[:id])
+        end
+      end
+    end
+
+    # Update the project's xml representation with the obj's project/owner id and name, if the project metadata
+    # indicates the project (in params) was a new project.
+    def update_xml sym, obj
+      obj.each do |k,v|
+        the_metadata = @project_metadata[sym]
+        if the_metadata[:new]
+          pat = the_metadata[k].to_s.gsub /\*/, '\*'
+          @project_xml.sub! /#{pat}/, v.to_s
+        end
+      end
+    end
+
+    # If the project is new, save it and update the xml to reflect the project.id; otherwise do nothing.
+    def save_project
+      if !@project.new_record?
+        true
+      else
+        @project.save.ifTrue do
+          meta_project = @project_metadata[:project]
+          meta_owner = @project_metadata[:owner]
+          update_xml :project, :id => @project.id, :name => meta_project[:name]
+          update_xml :owner,   :id => @project.id, :name => meta_owner[:name]
+        end
+      end
+    end
+
+    # Save the project content after saving the project if it needs to be saved.
+    def save_content
+      if save_project
+        meta_owner = @project_metadata[:owner]
+        @content = Bookshelf::ProjectContent.new({ project_id: @project.id, body: @project_xml, created_by: meta_owner[:id] })
+        @content.save
+      end
+    end
 
     #def projects
     #  respond_to do |format|
--- app/models/bookshelf/project.rb
+++ app/models/bookshelf/project.rb
@@ -1,11 +1,9 @@
 module Bookshelf
-
   class Project < ActiveRecord::Base
-    has_many :project_contents
+    has_many :project_contents, :dependent => :destroy
 
     def contents
       project_contents
     end
   end
-
 end
