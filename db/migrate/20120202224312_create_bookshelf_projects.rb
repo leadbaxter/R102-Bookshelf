5f6ba5e96216a56666c58fb2f716c6d9284b1fd4
Brian Jackson <bjackson@gmail.com>
2012-11-15 13:25:33 -0600

    moved save project contents functionality to its own module; updated controller to serve project_contents.body for xml requests

--- db/migrate/20120202224312_create_bookshelf_projects.rb
+++ db/migrate/20120202224312_create_bookshelf_projects.rb
@@ -3,7 +3,7 @@ class CreateBookshelfProjects < ActiveRecord::Migration
     create_table :bookshelf_projects do |t|
       t.string :project_name
       t.string :project_number
-      t.integer :mod_date
+      t.datetime :mod_date
       t.string :created_by
       t.boolean :archived
 
