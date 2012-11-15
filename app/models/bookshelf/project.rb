f25501079cf65b4fd8651da8ae6fa3c44edfd999
Brian Jackson <bjackson@gmail.com>
2012-11-15 16:01:09 -0600

    moved save project contents functionality to its own module; updated controller to serve project_contents.body for xml requests

--- app/models/bookshelf/project.rb
+++ app/models/bookshelf/project.rb
@@ -2,6 +2,12 @@ module Bookshelf
   class Project < ActiveRecord::Base
     has_many :project_contents, :dependent => :destroy
 
+    def as_json
+      (super :except => :mod_date).tap do |answer|
+        answer[:mod_date] = mod_date.to_i
+      end
+    end
+
     def contents
       project_contents
     end
