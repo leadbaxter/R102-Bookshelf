e4dabd05f6820da6268f8413919d9a8e4bf91c10
Brian Jackson <brianj2@gmail.com>
2013-06-04 13:43:37 -0500

    added project_key, project_type to Bookshelf::Project

--- db/migrate/20130604175635_add_project_key_and_project_type_to_bookshelf_projects.rb
+++ db/migrate/20130604175635_add_project_key_and_project_type_to_bookshelf_projects.rb
@@ -1,7 +1,7 @@
 class AddProjectKeyAndProjectTypeToBookshelfProjects < ActiveRecord::Migration
   def change
-    add_column :bookshelf_projects, :project_key, :string, :null => false # generated/encoded
-    add_column :bookshelf_projects, :project_type, :string, :null => false
+    add_column :bookshelf_projects, :project_key, :string, :null => false, :default => 'UNKNOWN' # generated/encoded
+    add_column :bookshelf_projects, :project_type, :string, :null => false, :default => 'UNKNOWN'
   end
   add_index :bookshelf_projects, :project_key
 end
